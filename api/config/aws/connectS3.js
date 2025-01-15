1e5d9cb9c5e48c6b091d3857b79ca11c179bb43f
pradipj <pradipj@dhitech.solutions>
2025-01-15 14:43:27 +0530

    added changes for the reder deploy 2

--- api/config/aws/connectS3.js
+++ api/config/aws/connectS3.js
@@ -3,7 +3,7 @@ console.log(process.env);
 const connectS3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY1,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
